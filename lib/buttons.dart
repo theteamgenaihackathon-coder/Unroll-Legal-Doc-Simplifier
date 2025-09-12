8081196555b38b52ae029ee12571e24ca0b36c04
theteamgenaihackathon-coder <theteamgenaihackathon@gmail.com>
2025-09-12 19:06:02 +0530

    Ready to use LoginPage and HomePage

---
+++ lib/buttons.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+const Color ourRed = Color(0xFFC10547);
+
+IconButton menuButton() {
+  return IconButton(
+    onPressed: () {},
+    icon: Icon(Icons.menu_rounded, color: ourRed, size: 50),
+    highlightColor: Colors.transparent,
+    visualDensity: VisualDensity(horizontal: 2, vertical: -2),
+  );
+}
+
+IconButton accountButton() {
+  // return IconButton(
+  //   onPressed: () {},
+  //   icon: Icon(Icons.account_circle_rounded, color: ourRed, size: 50),
+  // );
+
+  return IconButton(
+    onPressed: () {},
+    icon: Icon(Icons.account_circle_rounded, color: ourRed, size: 50),
+    visualDensity: VisualDensity(vertical: -2),
+    highlightColor: Colors.transparent,
+  );
+}
+
+// PopupMenuButton accountButton() {
+//   return PopupMenuButton(
+//     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+//     offset: Offset(0, 60),
+//     padding: EdgeInsets.only(bottom: 10, right: 20),
+//     icon: Icon(Icons.account_circle_rounded, color: ourRed, size: 50),
+//     itemBuilder: (BuildContext context) => [
+//       PopupMenuItem(
+//         value: 'history',
+//         child: Center(child: Text('History')),
+//       ),
+//       PopupMenuDivider(),
+//       PopupMenuItem(
+//         value: 'saved',
+//         child: Center(child: Text('Saved')),
+//       ),
+//       PopupMenuDivider(),
+//       PopupMenuItem(
+//         value: 'settings',
+//         child: Center(child: Text('Settings')),
+//         onTap: () {},
+//       ),
+//       PopupMenuDivider(),
+//       PopupMenuItem(
+//         value: 'logout',
+//         child: Center(child: Text('Logout')),
+//       ),
+//     ],
+//   );
+// }
+
+IconButton uploadButton() {
+  return IconButton(
+    onPressed: () {},
+    icon: Icon(Icons.arrow_circle_up_rounded, size: 80, color: ourRed),
+  );
+}
+
+IconButton cameraButton() {
+  return IconButton(
+    onPressed: () {},
+    icon: Icon(Icons.camera_alt_rounded, color: ourRed, size: 80),
+  );
+}
